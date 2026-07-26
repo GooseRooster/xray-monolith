TheLostInPlace/xray-monolith-pip

Possibility to provide custom epsilon for light and glow for spatial move optimization

src/Layers/xrRender/light.h
@@ -161,8 +161,8 @@ public:
 	virtual void set_volumetric_intensity(float fValue) { m_volumetric_intensity = ps_ssfx_volumetric.y; }
 	virtual void set_volumetric_distance(float fValue) { m_volumetric_distance = 1.0f; }
 
-	virtual void set_position(const Fvector& P);
-	virtual void set_rotation(const Fvector& D, const Fvector& R);
+	virtual void set_position(const Fvector& P, const float eps = EPS);
+	virtual void set_rotation(const Fvector& D, const Fvector& R, const float eps = EPS);
 	virtual void set_cone(float angle);
 	virtual void set_range(float R);
 

GooseRooster/xray-monolith

src/Layers/xrRender/light.h
@@ -133,8 +133,8 @@ public:
 	virtual void set_volumetric_intensity(float fValue) { m_volumetric_intensity = ps_ssfx_volumetric.y; }
 	virtual void set_volumetric_distance(float fValue) { m_volumetric_distance = 1.0f; }
 
-	virtual void set_position(const Fvector& P);
-	virtual void set_rotation(const Fvector& D, const Fvector& R);
+	virtual void set_position(const Fvector& P, const float eps = EPS_L);
+	virtual void set_rotation(const Fvector& D, const Fvector& R, const float eps = EPS_L);
 	virtual void set_cone(float angle);
 	virtual void set_range(float R);
 
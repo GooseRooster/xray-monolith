TheLostInPlace/xray-monolith-pip

Burer and poltergeist telekinesis fix

src/xrGame/ai/monsters/burer/burer_state_attack_tele.h
@@ -1,12 +1,40 @@
 #pragma once
 #include "../state.h"
 #include "../../../grenade.h"
+#include "../../../../xrPhysics/iphysicsshellholder.h"
+#include "../../../../xrPhysics/icolisiondamageinfo.h"
 
 
 template <typename Object>
 class CStateBurerAttackTele : public CState<Object>
 {
 	typedef CState<Object> inherited;
+
+    struct SCollisionHitCallback :
+        public ICollisionHitCallback
+    {
+        Object* m_object;
+        bool done;
+
+        SCollisionHitCallback(Object* obj)
+            : m_object(obj), done(false)
+        {}
+
+        void call(CObject*& obj, float min_cs, float max_cs, float& cs, float& hl, ICollisionDamageInfo* di) override
+        {
+            if (done) return;
+            done = true;
+
+            obj = nullptr;
+
+            if (cs > min_cs * 0.5f)
+            {
+                obj = m_object;
+            }
+
+            di->SetInitiated();
+        }
+    };
 
 	xr_vector<CPhysicsShellHolder *> tele_objects;
 	CPhysicsShellHolder* selected_object;

GooseRooster/xray-monolith

src/xrGame/ai/monsters/burer/burer_state_attack_tele.h
@@ -1,12 +1,40 @@
 #pragma once
 #include "../state.h"
 #include "../../../Grenade.h"
+#include "../../../../xrPhysics/IPhysicsShellHolder.h"
+#include "../../../../xrPhysics/IColisiondamageInfo.h"
 
 
 template <typename Object>
 class CStateBurerAttackTele : public CState<Object>
 {
 	typedef CState<Object> inherited;
+
+    struct SCollisionHitCallback :
+        public ICollisionHitCallback
+    {
+        Object* m_object;
+        bool done;
+
+        SCollisionHitCallback(Object* obj)
+            : m_object(obj), done(false)
+        {}
+
+        void call(CObject*& obj, float min_cs, float max_cs, float& cs, float& hl, ICollisionDamageInfo* di) override
+        {
+            if (done) return;
+            done = true;
+
+            obj = nullptr;
+
+            if (cs > min_cs * 0.5f)
+            {
+                obj = m_object;
+            }
+
+            di->SetInitiated();
+        }
+    };
 
 	xr_vector<CPhysicsShellHolder *> tele_objects;
 	CPhysicsShellHolder* selected_object;